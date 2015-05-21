9a47998cb0ffe2661af932d3bd7cd47707e68247
Sander <sander@van-der-wel.net>
2015-05-21 13:53:09 +0200

    update weather every minute

--- src/main.c
+++ src/main.c
@@ -37,6 +37,19 @@ static void update_time() {
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
+
+  // Get weather update every 30 minutes
+  if(tick_time->tm_min % 1 == 0) {
+    // Begin dictionary
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+
+    // Add a key-value pair
+    dict_write_uint8(iter, 0, 0);
+
+    // Send the message!
+    app_message_outbox_send();
+  }
 }
 
 void ConfigureTextLayer(GColor TextColor, GColor BackgroundColor, GColor dateTextColor, GColor dateBackgroundColor) {
